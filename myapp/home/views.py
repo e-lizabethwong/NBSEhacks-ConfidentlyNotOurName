16801d194777e9e1577cf99a2142033fc9157d59
seansongss <seansongss@gmail.com>
2024-02-24 18:40:01 -0500

    update

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from image_upload.models import ImageUpload
 
 def home(request):
-    return HttpResponse("Hello world!")
+    myimages = ImageUpload.objects.all().values()
+    template = loader.get_template('home.html')
+    context = {
+        'myimages': myimages,
+    }
+    return HttpResponse(template.render(context, request))
