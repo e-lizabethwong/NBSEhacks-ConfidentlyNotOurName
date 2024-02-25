8a9b87de2fb6864aef815871003e4f9c1fc6bd2e
seansongss <seansongss@gmail.com>
2024-02-25 04:41:47 -0500

    edit text display

--- myapp/cohereChat.py
+++ myapp/cohereChat.py
@@ -1,55 +1,30 @@
 import cohere
 
-ch_api_key = 'fVHw36A8DdVfH3ubOgu6bjQotk37QinmZLz4X0sq'
-
-# co = cohere.Client(ch_api_key)
-#
-# #input from user
-# message = "why is the sky blue?"
-#
-# response = co.chat(
-#     message,
-#     model="command",
-#     temperature=0.9#,
-#     #chat_history=
-# )
-#
-# answer = response.text
-# print(answer)
-
 def cohereChat(message, coach_type):
-    # coach_type = "Maria, a health and fitness coach"
+    ch_api_key = 'fVHw36A8DdVfH3ubOgu6bjQotk37QinmZLz4X0sq'
     chat_history = []
-    user_message = {"user_name": "User", "text": "You are " + coach_type}
     # max_turns = 10
-
+    chat_history = []
     co = cohere.Client(ch_api_key)
-
+    user_message = {"user_name": "User", "text": "You are " + coach_type}
+    # for _ in range(max_turns):
+    #     # get user input
+    #     # generate a response with the current chat history
+    
     response = co.chat(
-            message,
-            temperature=0.8,
-            chat_history=chat_history
-        )
-
+        message,
+        temperature=0.8,
+        chat_history=chat_history,
+        preamble_override="you give positive, polite, helpful, and inclusive advice based on facts",
+        connectors=[{"id": "web-search"}]
+    )
     answer = response.text
     
     return answer
-    # for _ in range(max_turns):
-    #     # get user input
-    #     message = input("Send the model a message: ")
 
-    #     # generate a response with the current chat history
-    #     response = co.chat(
-    #         message,
-    #         temperature=0.8,
-    #         chat_history=chat_history
-    #     )
-    #     answer = response.text
+        # # add message and answer to the chat history
+        # user_message = {"user_name": "User", "text": message}
+        # bot_message = {"user_name": "Chatbot", "text": answer}
 
-    #     # print(answer)
-
-    #     # # add message and answer to the chat history
-    #     # user_message = {"user_name": "User", "text": message}
-    #     # bot_message = {"user_name": "Chatbot", "text": answer}
-
-    #     return answer
+        # chat_history.append(user_message)
+        # chat_history.append(bot_message)
